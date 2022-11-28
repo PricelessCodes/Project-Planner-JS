1d153c8c4998d309330a60b30792bb166624106c
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2022-11-28 20:22:12 +0200

    Starting with the Switch Project Logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,46 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton();
+    }
+
+    connectMoreInfoButton() {}
+
+    connectSwitchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        const switchBtn = projectItemElement.querySelector(
+            "button:last-of-type"
+        );
+        switchBtn.addEventListener("click");
+    }
+}
 
 class ProjectList {
-    constructor(type) {
+    projects = [];
+    type = "";
+    switchHandlerFunction = null;
+    constructor(type, switchHandlerFunction) {
+        this.type = type;
+        this.switchHandlerFunction = switchHandlerFunction;
         const projectItems = document.querySelectorAll(`#${type}-projects li`);
-        ///asw
+        for (const pi of projectItems) {
+            this.projects.push(new ProjectItem(pi.id));
+        }
+    }
+
+    addProject() {}
+
+    switchProject(projectId) {
+        const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+        if (projectId !== -1) {
+            //add project to other array of projects
+            this.switchHandlerFunction(projects[projectIndex]);
+            //remove found project from this array
+            this.projects.splice(projectIndex, 1);
+        }
     }
 }
 
